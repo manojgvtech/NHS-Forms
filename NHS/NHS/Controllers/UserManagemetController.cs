d000d9dac9d6986c19791e99094354b757d29930
Mrityunjay Singh <mrityunjays@corp.gvtech.com>
2019-01-11 22:59:37 +0530

    User Management

---
+++ NHS/NHS/Controllers/UserManagemetController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NHS.Controllers
+{
+    public class UserManagemetController : Controller
+    {
+        // GET: UserManagemet
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult UserDetails()
+        {
+            return View();
+        }
+    }
+}
---
+++ NHS/NHS/Utility/Log.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NHS.Utility
+{
+    public static class Log
+    {
+    }
+}
